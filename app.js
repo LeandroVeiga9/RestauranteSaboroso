745a6b5f6d3ed4438d164b156e73003b53e553bf
LeandroVeiga9 <94861857+LeandroVeiga9@users.noreply.github.com>
2021-12-08 17:50:23 -0300

    add socket.io

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ var logger = require('morgan');
 var session = require('express-session')
 // var RedisStore = require('connect-redis')(session)
 var formidable = require('formidable')
+var http = require('http')
+var socket = require('socket.io')
 var path = require('path')
 
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
-
 var app = express();
+
+var http = http.Server(app)
+var io = socket(http)
+
+io.on('connection', function(socket){
+
+    console.log('novo usuario conectado');
+
+})
+
+var indexRouter = require('./routes/index')(io)
+var adminRouter = require('./routes/admin')(io)
 
 app.use(function(req, res, next){
 
@@ -77,4 +88,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+http.listen(3000, function(){
+
+    console.log('servidor em execução na porta 3000');
+
+})
